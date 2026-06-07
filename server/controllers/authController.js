9ba7a60488fc192f6f0cdd7c31b206d14f17655b
Dipanshu Soni <dipanshusoni369@gmail.com>
2026-06-07 08:15:43 +0530

    Created the login route with JWT token assignment.

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,4 +1,5 @@
 const User = require('../models/User');
+const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 
 const registerUser = async (req, res) => {
@@ -46,4 +47,51 @@ const registerUser = async (req, res) => {
     }
 };
 
-module.exports = registerUser;
+const loginUser = async (req, res) => {
+    try
+    {
+        const {email, password} = req.body;
+
+        if(!email || !password)
+        {
+            return res.status(400).json({
+                message: "All fields are required !"
+            });
+        }
+
+        const existingUser = await User.findOne({email});
+        if(!existingUser)
+        {
+            return res.status(404).json({
+                message: "User not found !"
+            });
+        }
+
+        const isMatch = await bcrypt.compare(password, existingUser.password);
+        if(!isMatch)
+        {
+            return res.status(401).json({
+                message: "Invalid Credentials !"
+            });
+        }
+
+        const token = jwt.sign(
+            {userId: existingUser._id},
+            process.env.JWT_SECRET,
+            {expiresIn: "1h"}
+        );
+
+        res.status(200).json({
+            message: "Login Successful !",
+            token: token
+        });
+    }
+    catch(error)
+    {
+        res.status(500).json({
+            message: "Error in logging in !"
+        });
+    }
+};
+
+module.exports = {registerUser, loginUser};
