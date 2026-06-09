20fd4817b8085be02d380966f1da62647fd715b0
Dipanshu Soni <dipanshusoni369@gmail.com>
2026-06-09 17:23:13 +0530

    implement complete frontend task CRUD operations.

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,20 +1,22 @@
-import {useEffect} from 'react';
+import {useEffect, useState} from 'react';
 import {useNavigate} from 'react-router-dom';
 import axios from 'axios';
 
 function Dashboard()
 {
     const navigate = useNavigate();
-    useEffect(() => {
-        const token = localStorage.getItem('token');
 
-        if(!token)
-            navigate('/');
-        else
-            fetchTasks();
-    }, [])
-
+    // Store all Tasks
     const [tasks, setTasks] = useState([]);
+
+    // Add task states
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+
+    // Edit task states
+    const [editTaskId, setEditTaskId] = useState(null);
+    const [editTitle, setEditTitle] = useState('');
+    const [editDescription, setEditDescription] = useState('');
 
     const fetchTasks = async () => {
         try
@@ -37,9 +39,6 @@ function Dashboard()
             console.log(error);
         }
     };
-
-    const [title, setTitle] = useState('');
-    const [decription, setDescription] = useState('');
 
     const addTask = async (e) => {
         e.preventDefault();
@@ -93,6 +92,45 @@ function Dashboard()
             console.log(error);
         }
     };
+
+    const updateTask = async (taskId) => {
+        try
+        {
+            const token = localStorage.getItem('token');
+
+            const updatedData = {
+                title: editTitle,
+                description: editDescription
+            }
+
+            await axios.put(
+                `http://localhost:5000/api/tasks/${taskId}`,
+                updatedData,
+                {
+                    headers: {
+                        Authorization: `Bearer ${token}`
+                    }
+                }
+            );
+
+            setEditTaskId(null);
+            fetchTasks();
+        }
+        catch(error)
+        {
+            console.log(error);
+        }
+    };
+
+    // Protect Dashboard
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+
+        if(!token)
+            navigate('/');
+        else
+            fetchTasks();
+    }, []);
     /* Everything inside return is UI shown on screen, while things
     outside return are backend/frontend logic handling.*/
     return (
@@ -108,24 +146,103 @@ function Dashboard()
 
                 <textarea
                     placeholder="Enter Description"
-                    name={description}
+                    value={description}
                     onChange={(e) => setDescription(e.target.value)}
                 />
 
                 <button type="submit">Add Task</button>
             </form>
+            <hr />
             {
                 // Task List
                 /*Arrow function is used in button tag because it waits until
                 delete button is clicked.*/
                 tasks.map((task) => (
-                    <div key={task._id}>
-                        <h3>{task.title}</h3>
-                        <p>{task.description}</p>
-                        <button onClick={() => deleteTask(task._id)}>Delete</button>
-                    </div>
-                ))
-            }
+                    <div
+                        key={task._id}
+                        style={{
+                            border: '1px solid black',
+                            padding: '10px',
+                            marginBottom: '10px'
+                        }}
+                    >
+                        
+                        {
+                            editTaskId === task._id
+                            ?
+                            (
+                                <div>
+                                    <input
+                                        type="text"
+                                        value={editTitle}
+                                        onChange={(e) => setEditTitle(e.target.value)}
+                                    />
+
+                                    <textarea
+                                        value={editDescription}
+                                        onChange={(e) =>
+                                            setEditDescription(e.target.value)
+                                        }
+                                    />
+
+                                    <button
+                                        onClick={() => updateTask(task._id)}
+                                    >
+                                        Update
+                                    </button>
+
+                                </div>
+                            ):(
+                                <div>
+
+                                    <h3>{task.title}</h3>
+
+                                    <p>{task.description}</p>
+
+                                    <p>
+                                        Status:
+                                        {
+                                            task.completed
+                                            ? ' Completed'
+                                            : ' Pending'
+                                        }
+                                    </p>
+
+                                </div>
+                            )
+                        }
+
+                        <button
+                            onClick={() => {
+                                setEditTaskId(task._id);
+                                setEditTitle(task.title);
+                                setEditDescription(task.description);
+                            }}
+                        >
+                            Edit
+                        </button>
+
+                        <button
+                            onClick={() =>
+                                toggleComplete(
+                                    task._id,
+                                    task.completed
+                                )
+                            }
+                        >
+                            {
+                                task.completed
+                                ? 'Undo'
+                                : 'Complete'
+                            }
+                        </button>
+
+                        <button onClick={() => deleteTask(task._id)}>
+                            Delete
+                        </button>
+                                            </div>
+                                        ))
+                                    }
         </div>
     );
 }
